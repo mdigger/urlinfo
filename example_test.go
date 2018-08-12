625294833d4952a2854f21d1c2551dfe60cd91e0
Dmitry Sedykh <dmitrys@xyzrd.com>
2018-08-12 17:25:04 +0300

    skip example test

--- example_test.go
+++ example_test.go
@@ -8,26 +8,23 @@ import (
 )
 
 func Example() {
-	info := urlinfo.Get("http://mdigger.tumblr.com")
+	info := urlinfo.Get("https://edition.cnn.com/2018/08/11/politics/donald-trump-vacation-new-jersey/index.html")
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "    ")
 	enc.Encode(info)
 	// Output:
 	// {
-	//     "url": "http://mdigger.tumblr.com",
-	//     "status": 200,
-	//     "mediaType": "text/html; charset=UTF-8",
-	//     "title": "Копилка ненужных вещей",
-	//     "description": "У каждого человека должна быть своя \"копилка ненужных вещей\", которой он с радостью готов...",
-	//     "image": "http://68.media.tumblr.com/avatar_d1b501c5ba67_128.png",
-	//     "keywords": [
-	//         "юмор",
-	//         "цитаты",
-	//         "чиполлино",
-	//         "интроверт",
-	//         "еда"
-	//     ],
-	//     "type": "tumblr-feed:tumblelog",
-	//     "site": "tumblr"
+	// 	"url": "https://edition.cnn.com/2018/08/11/politics/donald-trump-vacation-new-jersey/index.html",
+	// 	"status": 200,
+	// 	"mediaType": "text/html; charset=utf-8",
+	// 	"title": "Peek inside Trump's total non-vacation in New Jersey",
+	// 	"description": "For President Donald Trump, the long-standing image of a leader in repose doesn't apply, at least in his own mind. In New Jersey this week, events were arranged to demonstrate his extended stay at a wooded golf resort was, in fact, work.",
+	// 	"author": "Kevin Liptak and Jeff Zeleny, CNN",
+	// 	"image": "https://cdn.cnn.com/cnnnext/dam/assets/170611140113-trump-golfing-super-tease.jpg",
+	// 	"keywords": [
+	// 		"politics"
+	// 	],
+	// 	"type": "article",
+	// 	"site": "CNN"
 	// }
 }
